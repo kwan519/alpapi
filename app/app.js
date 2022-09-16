588a6ab3ed2c1501f5bd230983051201dc5cbe39
Kwan <kwan@werehumans.com>
2022-09-16 14:03:12 +0700

    jwt generated

--- app/app.js
+++ app/app.js
@@ -1,12 +1,18 @@
 import "@babel/polyfill";
 import express from "express";
-import resolvers from "./GraphQL/tickets";
 import tickets from "./sql/tickets";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 const bodyParser = require("body-parser");
 const { ApolloServer } = require("apollo-server-express");
 const cors = require("cors");
+
 const app = express();
+
+// Set up Global configuration access
+dotenv.config();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -25,6 +31,10 @@ async function startServer() {
 
 startServer();
 
+app.listen({ port: 5000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:5000`)
+);
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 app.get("/test", (req, res) => {
@@ -37,6 +47,14 @@ app.get("/test", (req, res) => {
   } 
 });
 
-app.listen({ port: 5000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:5000`)
-);
+app.post("/login", (req, res) => {
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  
+  let data = {  
+    time: Date(),
+    userId: 12
+  }
+
+  const token = jwt.sign(data,jwtSecretKey);
+  res.send(token)
+})
