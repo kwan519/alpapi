7ba10a5777764895cece361a341fd988eba5961c
Kwan <kwan@werehumans.com>
2022-10-10 11:43:52 +0700

    update site get all

--- app/RESTApi/adminControllers/site/index.js
+++ app/RESTApi/adminControllers/site/index.js
@@ -1,7 +1,9 @@
 import db from '../../../database'
+import WriteLogFile from '../../../helpers/writeLogFile'
 import permission from '../../../RESTApi/utilityController/permission'
 const { Op } = require('sequelize')
 
+const LOG_FILE_NAME = 'site_log_error'
 const AvailableSites = async (userId) => {
   const result = await db.access_sites.findAll({
     where: {
@@ -13,48 +15,56 @@ const AvailableSites = async (userId) => {
 }
 
 const SiteGet = async (req, res) => {
-  const userId = res.locals.userId
-  const siteList = await AvailableSites(userId)
+  try {
+    const userId = res.locals.userId
+    const siteList = await AvailableSites(userId)
 
-  const siteId = res.locals.siteId ?? req.body.siteId
+    const siteId = res.locals.siteId ?? req.body.siteId
 
-  const isAdmin = await permission.IsAdmin(res.locals.userId)
+    const isAdmin = res.locals.permission === 'admin'
 
-  if (siteList.includes(siteId) || isAdmin) {
-    const siteData = await db.sites.findByPk(siteId)
-    const siteSettingData = await db.site_settings.findOne({
-      where: {
-        sites_id: siteId
-      }
-    })
-    res.send({ siteId, data: siteData, setting: siteSettingData })
-  } else {
-    res.sendStatus(401)
+    if (siteList.includes(siteId) || isAdmin) {
+      const siteData = await db.sites.findByPk(siteId)
+      const siteSettingData = await db.site_settings.findOne({
+        where: {
+          sites_id: siteId
+        }
+      })
+      res.send({ status: 'success', data: { siteId, site: siteData, setting: siteSettingData } })
+    } else {
+      res.sendStatus(401)
+    }
+  } catch (error) {
+    WriteLogFile(LOG_FILE_NAME, `siteGet: ${error}`)
+    res.sendStatus(500)
   }
 }
 
 const SiteGetAll = async (req, res) => {
-  console.log(res.locals)
-
-  let options = {
-    status: {
-      [Op.notIn]: ['deleted']
-    }
-  }
-  if (res.locals.permission !== 'admin') {
-    const siteAvailabelList = await AvailableSites(res.locals.userId)
-    options = {
-      ...options,
-      id_site: {
-        [Op.in]: [...siteAvailabelList]
+  try {
+    let options = {
+      status: {
+        [Op.notIn]: ['deleted']
       }
     }
-  }
-  const data = await db.sites.findAll({
-    where: { ...options }
-  })
+    if (res.locals.permission !== 'admin') {
+      const siteAvailabelList = await AvailableSites(res.locals.userId)
+      options = {
+        ...options,
+        id_site: {
+          [Op.in]: [...siteAvailabelList]
+        }
+      }
+    }
+    const data = await db.sites.findAll({
+      where: { ...options }
+    })
 
-  res.send({ status: 'success', data })
+    res.send({ status: 'success', data })
+  } catch (error) {
+    WriteLogFile(LOG_FILE_NAME, `siteGetAll: ${error}`)
+    res.sendStatus(500)
+  }
 }
 
 const SiteCreate = async (req, res) => {
